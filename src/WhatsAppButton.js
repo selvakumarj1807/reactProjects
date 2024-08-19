aa99dfcbb02f812268c4026baf7cc721b171c019
selva <selvakumarj1807@gmail.com>
2024-08-19 15:47:19 +0530

    whatsapp

---
+++ src/WhatsAppButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import whatsappImage from './assets/images/WhatsAppButtonGreenLarge.png'; // Adjust the path accordingly
+import './WhatsAppButton.css'; // Ensure the correct path to your CSS file
+
+function WhatsAppButton() {
+  return (
+    <div className="whatsapp-button-container">
+      <a
+        aria-label="Chat on WhatsApp"
+        href="https://wa.me/9500912258?text=Hello%20there!"
+      >
+        <img
+          alt="Chat on WhatsApp"
+          src={whatsappImage}
+          className="responsive-img"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default WhatsAppButton;
