553ba1c77396e96128b4b1e7969c4b68c3684dc0
selva <selvakumarj1807@gmail.com>
2024-08-20 16:22:35 +0530

    2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import WhatsAppButton from './WhatsAppButton'; // Adjust the import path as need
 
 import Contact from './Components/Contact/Contact';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Email from './Components/Email'
+
 function App() {
   return (
     <div className="App">
-      <Contact />
-      <WhatsAppButton />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Email />} />
+          <Route path="/send-email" element={<Contact />} />
+        </Routes>
+        <WhatsAppButton />
+      </Router>
     </div>
   );
 }
